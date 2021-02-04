84a547c3394aa9efe0c77e20bbd90a1dcaec6462
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-02-04 18:42:37 -0500

    add try catch to enrich

--- src/PlayerTrack/Service/PlayerService/PlayerService.Enrich.cs
+++ src/PlayerTrack/Service/PlayerService/PlayerService.Enrich.cs
@@ -8,30 +8,53 @@ namespace PlayerTrack
 	{
 		public void EnrichPlayerData(TrackPlayer player)
 		{
-			AddWorldData(player);
-			AddCustomizeData(player);
-			AddLocationData(player);
-			AddCategoryData(player);
-			AddIconData(player);
-			player.PreviouslyLastSeen = player.LastSeen;
+			try
+			{
+				AddWorldData(player);
+				AddCustomizeData(player);
+				AddLocationData(player);
+				AddCategoryData(player);
+				AddIconData(player);
+				player.PreviouslyLastSeen = player.LastSeen;
+			}
+			catch (Exception ex)
+			{
+				_plugin.LogError(ex, "Failed to add data to player " + player.Key);
+			}
 		}
 
 		private void AddWorldData(TrackPlayer player)
 		{
-			foreach (var world in player.HomeWorlds)
-				world.Name = _plugin.GetWorldName(world.Id);
+			try
+			{
+				foreach (var world in player.HomeWorlds)
+					world.Name = _plugin.GetWorldName(world.Id);
+			}
+			catch (Exception ex)
+			{
+				_plugin.LogError(ex, "Failed to add world data to player " + player.Key);
+			}
+
 		}
 
 		private void AddCustomizeData(TrackPlayer player)
 		{
-			if (player.Gender == null) return;
-			var gender = (int) player.Gender;
-			player.GenderDisplay = _plugin.GetGender(gender);
-			player.RaceDisplay = _plugin.GetRace(player.Race, gender);
-			player.TribeDisplay = _plugin.GetTribe(player.Tribe, gender);
-			player.HeightDisplay =
-				_plugin.ConvertHeightToInches(player.Race, player.Tribe, gender, player.Height)
-					.ToString(CultureInfo.CurrentCulture);
+			try
+			{
+				if (player.Gender == null) return;
+				var gender = (int) player.Gender;
+				player.GenderDisplay = _plugin.GetGender(gender);
+				player.RaceDisplay = _plugin.GetRace(player.Race, gender);
+				player.TribeDisplay = _plugin.GetTribe(player.Tribe, gender);
+				player.HeightDisplay =
+					_plugin.ConvertHeightToInches(player.Race, player.Tribe, gender, player.Height)
+						.ToString(CultureInfo.CurrentCulture);
+			}
+			catch (Exception ex)
+			{
+				_plugin.LogError(ex, "Failed to add customize data to player " + player.Key);
+			}
+
 		}
 
 		private void AddLocationData(TrackPlayer player)
@@ -58,20 +81,35 @@ namespace PlayerTrack
 
 		private void AddCategoryData(TrackPlayer player)
 		{
-			var categoryIds = _plugin.CategoryService.CategoryIds;
-			if (player.CategoryId != 0 && !categoryIds.Contains(player.CategoryId))
-				player.CategoryId = 0;
-			player.Category = player.CategoryId == 0
-				? _plugin.CategoryService.GetDefaultCategory()
-				: _plugin.CategoryService.GetCategory(player.CategoryId);
-			player.CategoryIndex = _plugin.CategoryService.GetCategoryIndex(player.CategoryId);
-			var categoryPriorities = _plugin.CategoryService.CategoryPriorities;
-			player.Priority = categoryPriorities.FirstOrDefault(pair => pair.Key == player.CategoryId).Value;
+			try
+			{
+				var categoryIds = _plugin.CategoryService.CategoryIds;
+				if (player.CategoryId != 0 && !categoryIds.Contains(player.CategoryId))
+					player.CategoryId = 0;
+				player.Category = player.CategoryId == 0
+					? _plugin.CategoryService.GetDefaultCategory()
+					: _plugin.CategoryService.GetCategory(player.CategoryId);
+				player.CategoryIndex = _plugin.CategoryService.GetCategoryIndex(player.CategoryId);
+				var categoryPriorities = _plugin.CategoryService.CategoryPriorities;
+				player.Priority = categoryPriorities.FirstOrDefault(pair => pair.Key == player.CategoryId).Value;
+			}
+			catch (Exception ex)
+			{
+				_plugin.LogError(ex, "Failed to add category data to player " + player.Key);
+			}
 		}
 
 		private void AddIconData(TrackPlayer player)
 		{
-			player.IconIndex = Array.IndexOf(_plugin.GetIconCodes(), player.DisplayIcon);
+			try
+			{
+				player.IconIndex = Array.IndexOf(_plugin.GetIconCodes(), player.DisplayIcon);
+			}
+			catch (Exception ex)
+			{
+				_plugin.LogError(ex, "Failed to add icon data to player " + player.Key);
+			}
+
 		}
 	}
 }
