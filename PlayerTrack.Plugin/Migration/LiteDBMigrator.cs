962b08de4d5b8d475de0a48896ab7c135d94b47b
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-10-01 14:17:03 -0400

    fix: migrate visibility type

--- PlayerTrack.Plugin/Migration/LiteDBMigrator.cs
+++ PlayerTrack.Plugin/Migration/LiteDBMigrator.cs
@@ -355,10 +355,15 @@ public static class LiteDBMigrator
         var hasNonDefaultSetting = false;
         var playerConfig = new PlayerConfig(PlayerConfigType.Player) { PlayerId = player.Id };
 
-        var visibilityType = oldestPlayer.GetValueOrDefault<VisibilityType>("VisibilityType");
-        if (visibilityType != VisibilityType.None)
+        var visibilityType = 0;
+        if (oldestPlayer.TryGetValue("VisibilityType", out var bsonValue))
         {
-            playerConfig.VisibilityType = new ConfigValue<VisibilityType>(InheritOverride.Override, visibilityType);
+            visibilityType = (int)bsonValue.RawValue;
+        }
+
+        if (visibilityType > 0)
+        {
+            playerConfig.VisibilityType = new ConfigValue<VisibilityType>(InheritOverride.Override, (VisibilityType)visibilityType);
             hasNonDefaultSetting = true;
         }
 
@@ -468,10 +473,15 @@ public static class LiteDBMigrator
             hasNonDefaultSetting = true;
         }
 
-        var visibilityType = originalCategory.GetValueOrDefault<VisibilityType>("VisibilityType");
-        if (visibilityType != VisibilityType.None)
+        var visibilityType = 0;
+        if (originalCategory.TryGetValue("VisibilityType", out var bsonValue))
         {
-            playerConfig.VisibilityType = new ConfigValue<VisibilityType>(InheritOverride.Override, visibilityType);
+            visibilityType = (int)bsonValue.RawValue;
+        }
+
+        if (visibilityType > 0)
+        {
+            playerConfig.VisibilityType = new ConfigValue<VisibilityType>(InheritOverride.Override, (VisibilityType)visibilityType);
             hasNonDefaultSetting = true;
         }
 
