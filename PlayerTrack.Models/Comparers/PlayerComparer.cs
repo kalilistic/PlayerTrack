ded4b9dee8c20b263ed66068fc91bd95c36f05d1
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-11-21 14:57:20 -0500

    fix: add back comparer try catch

--- PlayerTrack.Models/Comparers/PlayerComparer.cs
+++ PlayerTrack.Models/Comparers/PlayerComparer.cs
@@ -16,20 +16,29 @@ public class PlayerComparer : IComparer<Player>
 
     public int Compare(Player? x, Player? y)
     {
-        if (ReferenceEquals(x, y)) return 0;
-        if (ReferenceEquals(null, y)) return 1;
-        if (ReferenceEquals(null, x)) return -1;
-
-        var xRank = this.CategoryRanks.TryGetValue(x.PrimaryCategoryId, out var rank) ? rank : DefaultRank;
-        var yRank = this.CategoryRanks.TryGetValue(y.PrimaryCategoryId, out var categoryRank) ? categoryRank : DefaultRank;
-        
-        var categoryComparison = xRank.CompareTo(yRank);
-        if (categoryComparison != 0)
+        try
         {
-            return categoryComparison;
-        }
+            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(null, y)) return 1;
+            if (ReferenceEquals(null, x)) return -1;
 
-        var nameComparison = string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
-        return nameComparison != 0 ? nameComparison : x.WorldId.CompareTo(y.WorldId);
+            var xRank = this.CategoryRanks.TryGetValue(x.PrimaryCategoryId, out var rank) ? rank : DefaultRank;
+            var yRank = this.CategoryRanks.TryGetValue(y.PrimaryCategoryId, out var categoryRank)
+                ? categoryRank
+                : DefaultRank;
+
+            var categoryComparison = xRank.CompareTo(yRank);
+            if (categoryComparison != 0)
+            {
+                return categoryComparison;
+            }
+
+            var nameComparison = string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+            return nameComparison != 0 ? nameComparison : x.WorldId.CompareTo(y.WorldId);
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
     }
 }
